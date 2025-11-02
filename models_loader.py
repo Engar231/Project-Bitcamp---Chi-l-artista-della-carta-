488c5a7f7c8dd5910e783c752b1a33848005ff67
Alfio Ezechiele Cavallaro <calfio23@gmail.com>
2025-11-02 02:31:43 +0100

    Add files via upload

---
+++ models_loader.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.models import load_model
+# _____________________________________________CONFIG PATH_____________________________________________
+MODELLO_MACRO_PATH = r"D:\Programmazione\Python BitCamp\Python\Progetto Alfio - Riconoscimento Carte\dataset\BestMacro.keras"
+MODELLO_BRISCOLA_PATH = r"D:\Programmazione\Python BitCamp\Python\Progetto Alfio - Riconoscimento Carte\dataset\carte_briscola\miglior_modello_briscola.keras"
+
+# _____________________________________________FUNZIONI DI CARICAMENTO_____________________________________________
+def get_modello(tipo="macro"):
+    if tipo == "macro":
+        return load_model(MODELLO_MACRO_PATH)
+    elif tipo == "briscola":
+        return load_model(MODELLO_BRISCOLA_PATH)
+    else:
+        raise ValueError(f"Tipo modello non valido: {tipo}")
+
